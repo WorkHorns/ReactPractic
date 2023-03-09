0bcf6a9e8d72213315a76858c3f16340d3b9e404
SergeevA <workhorns@gmail.com>
2023-03-09 17:01:33 +0700

    React install webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// import {Slider,AutoSlider,button} from './script';
+import Slider from "../script/script";
+
+const slider = new Slider(123,123,24);
+slider.whoAmI();
---
+++ src/script.js
@@ -0,0 +1,46 @@
+function button(){
+    return 'button';
+}
+
+class Slider {
+    constructor(widht, height, count) {
+        this.widht = widht;
+        this.height = height;
+        this.count = count
+    }
+    nestSlide(){
+        console.log('Move');
+    }
+
+    prevSlide(){
+        console.log('Back');
+    }
+
+    whoAmI(){
+        console.log(`Я толще на ${this.widht},и выше на ${this.height},и мое любимое число ${this.count}`);
+    }
+}
+
+// const slider = new Slider(100, 200, 10),
+//     someSlider = new Slider(150, 250, 1);  
+
+// slider.whoAmI();
+// someSlider.whoAmI();
+
+class AutoSlider extends Slider {
+    constructor(widht, height, count, auto) {
+        super(widht, height, count);
+        this.auto = auto;
+    }
+    play(){
+        console.log(`Autoplay: ${this.auto}`);
+    }
+}
+
+// const slider = new AutoSlider(100, 200, 10, true);
+
+// slider.whoAmI();
+// slider.play();
+
+export {Slider,AutoSlider,button}; //экспорт нескольких методов.
+export default Slider; //экспорт по умолчанию. Только один по умолчанию
